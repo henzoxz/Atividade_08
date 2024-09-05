6a30a5074e6f22ef4ef2cbc541c3e679d5bc0821
henzoxz <henzomag2009@gmail.com>
2024-09-05 09:59:20 -0300

    real_em_dolar

---
+++ atividade08.py
@@ -0,0 +1,5 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+dinheiroreal = int(input( " Digite o Valor em Real "))
+conversao = dinheiroreal/5.62
+
+print (conversao)
